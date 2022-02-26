e29eff082d54f3e385e03d20806ac10535a54b40
DevEstacion <DevEstacion@users.noreply.github.com>
2022-02-25 23:14:32 -0800

    Update the360dude-360image-debug.js

--- the360dude-360image-debug.js
+++ the360dude-360image-debug.js
@@ -1,3 +1,9 @@
 {   
-    "hotSpotDebug": true
+  "hotSpotDebug": true,
+  "mouseZoom": "fullscreenonly",
+  "autoLoad": true,
+  "author": "@ron.the.go"
+  "authorUrl": "https://instagram.com/ron.the.go",
+  "autoRotate": 1,
+  "autoRotateInactivityDelay": 2000
 }
